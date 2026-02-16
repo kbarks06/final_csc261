e7a06eb3dd846a41cbfc1781fe8f06cf123bacfc
kbarks06 <kbarker@mail.endicott.edu>
2026-02-16 15:26:07 -0500

    Initial setup

---
+++ src/main/java/com/mycompany/final_csc261/Final_csc261.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.final_csc261;
+
+/**
+ *
+ * @author kbark
+ */
+public class Final_csc261 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
